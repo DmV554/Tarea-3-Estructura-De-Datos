65c113ee9bf4938a6c450b91a4cb0cf15b04946c
DmV554 <daniel3318bl@gmail.com>
2023-05-20 19:53:47 -0400

    Archivos y Struct a ocupar

--- main.c
+++ main.c
@@ -3,10 +3,65 @@
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h> 
+#include "list.h"
+#include "heap.h"
+#include "treemap.h"
+#include "stack.h"
+
+typedef struct {
+  char nombre[31];
+  int prioridad;
+  List*listaTareasPrecedentes;
+  bool explorada;
+}Tarea;
+
+typedef struct {
+  char nombre[31]; 
+  bool completada;
+}TareaPrecedente;
+
+/*
+  función para comparar claves de tipo string
+  retorna 1 si son iguales
+*/
+int is_equal_string(void * key1, void * key2) {
+    if(strcmp((char*)key1, (char*)key2)==0) return 1;
+    return 0;
+}
+
+/*
+  función para comparar claves de tipo string
+  retorna 1 si son key1<key2
+*/
+int lower_than_string(void * key1, void * key2) {
+    if(strcmp((char*)key1, (char*)key2) < 0) return 1;
+    return 0;
+}
+
+/*
+  función para comparar claves de tipo int
+  retorna 1 si son iguales
+*/
+int is_equal_int(void * key1, void * key2) {
+    if(*(int*)key1 == *(int*)key2) return 1;
+    return 0;
+}
+
+/*
+  función para comparar claves de tipo int
+  retorna 1 si son key1<key2
+*/
+int lower_than_int(void * key1, void * key2) {
+    if(*(int*)key1 < *(int*)key2) return 1;
+    return 0;
+}
+
 
 int main() {
     char opcion[11];
     bool ejecucion = true;
+
+    TreeMap *mapaTareas = createTreeMap(lower_than_string);
 
    while (ejecucion) {
     printf("\n1.-AGREGAR TAREA\n");
